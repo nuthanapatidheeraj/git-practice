f79e359b165e97b3611867e032f9a4761f9f066c
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-01 15:56:44 +0530

    added conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh :: Hi Suresh,How are you?"
+echo "suresh :: Hello  ramesh .I am good.How are you doing?"
+echo "ramesh :: I am doing good suresh.What's going on?"
+echo "suresh :: I started learning shell script ramesh"
