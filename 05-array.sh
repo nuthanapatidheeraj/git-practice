a49e35c5336428c15eb967047f1102f046c97556
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-01 16:36:02 +0530

    I have given input to varaible

---
+++ 05-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#index starts from 0,size is 3
+FRUITS = ("APPLE" "KIWI" "ORANGE")
+
+echo "First fruit is : ${Fruits[0]}"
+echo "First fruit is : ${Fruits[1]}"
+echo "First fruit is : ${Fruits[2]}"
+
+echo "First fruit are : ${Fruits[@]}"
