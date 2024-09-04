c8043e3c2aae678d6659201b3cb51555c7957b08
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-04 21:43:51 +0530

    working on datatypes

--- 09-specialvars.sh
+++ 09-specialvars.sh
@@ -5,3 +5,6 @@ echo "Number of variables passed: $#"
 echo "Script Name: $0"
 echo "Curremt working directory: $PWD"
 echo "Home directory of current user: $HOME"
+echo "curremt script process instace id: $$"
+sleep 100 &
+echo "PID of last background command: $!"
