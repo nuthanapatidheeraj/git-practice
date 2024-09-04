e07d1b4a241f8a63b8937bd6c0aef8068f6a3b4a
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-04 23:00:33 +0530

    working on datatypes

---
+++ 10-conditions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -gt 20 ] #gt/lt,eq,!eq,<eq,>eq
+then
+   echo "Given number: $NUMBER is greater than 20"
+else
+   echo "Given number: $NUMBER is less tham 20"
+fi      
+
+
