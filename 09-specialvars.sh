d26ad327c6ffab13754c696ef6528b53cff3a7e0
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-04 21:00:50 +0530

    working on datatypes

---
+++ 09-specialvars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "All variables passed to the script : $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Curremt working directory: $PWD"
+echo "Home directory of current user: $HOME"
