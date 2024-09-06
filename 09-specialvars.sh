93d1e4827eb7ebd5f6665e0af79b72917d8c116e
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-06 12:29:29 +0530

    shell

--- 09-specialvars.sh
+++ 09-specialvars.sh
@@ -2,7 +2,7 @@
 
 echo "All variables passed to the script : $@"
 echo "Number of variables passed: $#"
-echo "Script Name: $0"
+echo "Script Name: $0" 
 echo "Curremt working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "curremt script process instace id: $$"
---
+++ 11-instllation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+echo "USER ID is: $USERID"
