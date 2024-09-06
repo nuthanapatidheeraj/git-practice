95412fb24cf48643e90afb8c9a4eb1a5075d5c87
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-06 12:42:57 +0530

    installation

--- 11-instllation.sh
+++ 11-instllation.sh
@@ -2,4 +2,14 @@
 
 USERID=$(id -u)
 
-echo "USER ID is: $USERID"
+#echo "USER ID is: $USERID"
+
+
+if[ USERID -ne 0 ]
+then 
+    echo"Please run the script with root previlages"
+    exit 1
+fi
+
+
+dnf install git -y
