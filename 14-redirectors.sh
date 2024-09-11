7fa5ebdbc75e670fadbf6fb49e810d9c84922ddc
nuthanapatidheeraj <dheerajnp200019@gmail.com>
2024-09-11 12:17:21 +0530

    redirectors

--- 14-redirectors.sh
+++ 14-redirectors.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d ". -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.LOG"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
-    IF [ $USERID -ne 0 ]
+    if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root previlages $N" &>>$LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
-    f1
+    fi
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo "$2 is ..$R FAILED $N" &>>$LOG_FILE
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
-    else 
-        echo "$2 is .. $G success $N" &>>$LOG_FILE
-    fi        
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
 USAGE(){
-    echo "USAGE:: sudo sh 16-redirectors.sh package1 package 2..."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
 }
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
 if [ $# -eq 0 ]
-then 
-    usage
-fi         
+then
+    USAGE
+fi
 
-for package in $@ #@referes to all the arguments passed to it
-do 
+for package in $@ # $@ refers to all arguments passed to it
+do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it.." &>>$LOG_FILE
+        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
         dnf install $package -y &>>$LOG_FILE
-        validate $? "Installing $package"
+        VALIDATE $? "Installing $package"
     else
-        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
-    fi 
-done  
+        echo -e "$package is already $Y installed..nothing to do $N" | tee -a $LOG_FILE
+    fi
+done
